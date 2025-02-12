c0eee8ba1fe6f6051a1be5e28acabc23cd3a68fb
midori <thaleth@live.com.mx>
2025-02-11 19:19:52 -0600

    Se modificó el JS

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigo;
+
+function agregarAmigo(){
+    nombreAmigo = document.getElementById("amigo").value;
+    console.log(`Nombre: ${nombreAmigo}`);
+    return;
+}
+
