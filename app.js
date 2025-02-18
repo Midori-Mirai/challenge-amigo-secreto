eea995f2baa4300826bef02f7274ae6fc9be0fc4
midori <thaleth@live.com.mx>
2025-02-18 13:15:46 -0600

    Se agregó recursividad y se finalizó el código

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let numAmigo;
 let listaAmigos = [];
 let mostrarLista = document.getElementById('showAmigos');
 let resultado = document.getElementById('resultado');
+let amigosSorteados = [];
 
 function limpiarListaAmigos(){
     mostrarLista.innerHTML = ""
@@ -11,9 +12,9 @@ function limpiarListaAmigos(){
 
 function actualizaLista(){
     for (let i = 0; i < listaAmigos.length; i++) {
-        let elemento = document.createElement("li"); // Crear un elemento <li>
-        elemento.textContent = listaAmigos[i]; // Asignar el nombre del amigo
-        mostrarLista.appendChild(elemento); // Agregar el <li> a la lista <ul>
+        let elemento = document.createElement("li"); // Creamos un elemento <li> para la lista
+        elemento.textContent = listaAmigos[i]; // Asignamos el nombre del amigo al elemento <li>
+        mostrarLista.appendChild(elemento); // Mostramos el <li> a la lista <ul> para actualizarla
     }
 }
 
@@ -38,10 +39,24 @@ function sortearAmigo(){
     numAmigo = listaAmigos.length;
     console.log(`tienes ${numAmigo} ${(numAmigo == 1) ? 'amigo' : 'amigos'}`)
     if(numAmigo !=0){
-        indiceSorteado = Math.floor(Math.random()*numAmigo)+1; //Generamos el índice aleatorio
-        console.log(`El numero aleatorio es: ${indiceSorteado}`);
-        console.log(`El amigo secreto es: ${listaAmigos[indiceSorteado-1]}`);
-        resultado.innerHTML = `El amigo secreto es ${listaAmigos[indiceSorteado-1]}`;
+        if(numAmigo == amigosSorteados.length){
+            alert('Ya no hay más amigos que sortear');
+        }
+        else{
+            indiceSorteado = Math.floor(Math.random()*numAmigo); //Generamos el índice aleatorio para nuestro amigo, no le sumamos el uno porque necesitamso el indice 0
+            //console.log(`El numero aleatorio es: ${indiceSorteado}`);
+            //console.log(`El amigo secreto es: ${listaAmigos[indiceSorteado]}`);
+            resultado.innerHTML = `El amigo secreto es ${listaAmigos[indiceSorteado]}`;
+            //Si el amigo ya salió, llamar función sortearAmigo, sino
+            if(amigosSorteados.includes(indiceSorteado)){
+                return sortearAmigo();
+            }
+            else{//sino agergar al amigo a la lista de amigos sorteados y regresar el índice para seguir
+                amigosSorteados.push(indiceSorteado);//Guardar el amigo sorteado
+                return indiceSorteado;
+            }
+        }
+        
     }
     else{
         alert(`No has añadido amigos aún`);
@@ -49,4 +64,3 @@ function sortearAmigo(){
 
     
 }
-
